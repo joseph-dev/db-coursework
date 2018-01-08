a6db1fc252c2a423497800bdacdfe4ea8e23e8c5
Joseph <yosypmykhailiv@gmail.com>
2018-01-08 20:15:04 +0200

    Resolved running migrations before executing web request and added BaseController

---
+++ controllers/BaseController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace app\controllers;
+
+use yii\filters\VerbFilter;
+use yii\web\Controller;
+
+class BaseController extends Controller
+{
+
+    /**
+     * @inheritdoc
+     */
+    public function behaviors()
+    {
+        return [
+            'verbs' => [
+                'class'   => VerbFilter::className(),
+                'actions' => [
+                    'delete' => ['post'],
+                ],
+            ],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function actions()
+    {
+        return [
+            'error' => [
+                'class' => 'yii\web\ErrorAction',
+            ]
+        ];
+    }
+}
--- web/index.php
+++ web/index.php
@@ -3,10 +3,17 @@
 // comment out the following two lines when deployed to production
 defined('YII_DEBUG') or define('YII_DEBUG', true);
 defined('YII_ENV') or define('YII_ENV', 'dev');
+defined('APP_MODE') or define('APP_MODE', 'modern');
+// value of APP_MODE can be legacy or modern
 
 require __DIR__ . '/../vendor/autoload.php';
 require __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
 
-$config = require __DIR__ . '/../config/web.php';
+// Run migration
+$config = require __DIR__ . '/../config/console.php';
+new \yii\console\Application($config);
+\Yii::$app->runAction('migrate/up', ['interactive' => false]);
 
+// Run basic web application
+$config = require __DIR__ . '/../config/web.php';
 (new yii\web\Application($config))->run();
