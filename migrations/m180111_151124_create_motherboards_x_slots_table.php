5f632e84816f32aef46452392880a050e04deaee
Joseph <yosypmykhailiv@gmail.com>
2018-01-15 22:38:38 +0200

    Fixed typo in migrations

--- migrations/m180111_151124_create_motherboards_x_slots_table.php
+++ migrations/m180111_151124_create_motherboards_x_slots_table.php
@@ -3,9 +3,9 @@
 use yii\db\Migration;
 
 /**
- * Handles the creation of table `motherboadrs_x_slots`.
+ * Handles the creation of table `motherboards_x_slots`.
  */
-class m180111_151124_create_motherboadrs_x_slots_table extends Migration
+class m180111_151124_create_motherboards_x_slots_table extends Migration
 {
     /**
      * @inheritdoc
@@ -15,22 +15,22 @@ class m180111_151124_create_motherboadrs_x_slots_table extends Migration
         if (APP_MODE === 'legacy') {
 
             $this->execute(
-                'CREATE TABLE `motherboadrs_x_slots` (
+                'CREATE TABLE `motherboards_x_slots` (
                     `motherboard_id` int(11) NOT NULL,
                     `slot_id` int(11) NOT NULL,
                     `quantity` int(11) NOT NULL
                 )'
             );
 
-            $this->execute('ALTER TABLE `motherboadrs_x_slots` ADD UNIQUE INDEX `idx-unique-mb_x_slots-mb_id-slot_id` (`motherboard_id`, `slot_id`)');
+            $this->execute('ALTER TABLE `motherboards_x_slots` ADD UNIQUE INDEX `idx-unique-mb_x_slots-mb_id-slot_id` (`motherboard_id`, `slot_id`)');
 
-            $this->execute('ALTER TABLE `motherboadrs_x_slots` ADD CONSTRAINT `fk-motherboadrs_x_slots-motherboard_id` FOREIGN KEY (`motherboard_id`) REFERENCES `motherboards` (`id`) ON DELETE CASCADE');
+            $this->execute('ALTER TABLE `motherboards_x_slots` ADD CONSTRAINT `fk-motherboards_x_slots-motherboard_id` FOREIGN KEY (`motherboard_id`) REFERENCES `motherboards` (`id`) ON DELETE CASCADE');
 
-            $this->execute('ALTER TABLE `motherboadrs_x_slots` ADD CONSTRAINT `fk-motherboadrs_x_slots-slot_id` FOREIGN KEY (`slot_id`) REFERENCES `slots` (`id`) ON DELETE CASCADE');
+            $this->execute('ALTER TABLE `motherboards_x_slots` ADD CONSTRAINT `fk-motherboards_x_slots-slot_id` FOREIGN KEY (`slot_id`) REFERENCES `slots` (`id`) ON DELETE CASCADE');
 
         } else {
 
-            $this->createTable('motherboadrs_x_slots', [
+            $this->createTable('motherboards_x_slots', [
                 'motherboard_id' => $this->integer()->notNull(),
                 'slot_id'        => $this->integer()->notNull(),
                 'quantity'       => $this->integer()->notNull(),
@@ -38,14 +38,14 @@ class m180111_151124_create_motherboadrs_x_slots_table extends Migration
 
             $this->createIndex(
                 'idx-unique-mb_x_slots-mb_id-slot_id',
-                'motherboadrs_x_slots',
+                'motherboards_x_slots',
                 ['motherboard_id', 'slot_id'],
                 true
             );
 
             $this->addForeignKey(
-                'fk-motherboadrs_x_slots-motherboard_id',
-                'motherboadrs_x_slots',
+                'fk-motherboards_x_slots-motherboard_id',
+                'motherboards_x_slots',
                 'motherboard_id',
                 'motherboards',
                 'id',
@@ -53,8 +53,8 @@ class m180111_151124_create_motherboadrs_x_slots_table extends Migration
             );
 
             $this->addForeignKey(
-                'fk-motherboadrs_x_slots-slot_id',
-                'motherboadrs_x_slots',
+                'fk-motherboards_x_slots-slot_id',
+                'motherboards_x_slots',
                 'slot_id',
                 'slots',
                 'id',
@@ -72,12 +72,12 @@ class m180111_151124_create_motherboadrs_x_slots_table extends Migration
         if (APP_MODE === 'legacy') {
 
             $this->execute(
-                'DROP TABLE `motherboadrs_x_slots`'
+                'DROP TABLE `motherboards_x_slots`'
             );
 
         } else {
 
-            $this->dropTable('motherboadrs_x_slots');
+            $this->dropTable('motherboards_x_slots');
 
         }
     }
--- migrations/m180112_141250_create_motherboards_x_external_ports_table.php
+++ migrations/m180112_141250_create_motherboards_x_external_ports_table.php
@@ -3,9 +3,9 @@
 use yii\db\Migration;
 
 /**
- * Handles the creation of table `motherboadrs_x_external_ports`.
+ * Handles the creation of table `motherboards_x_external_ports`.
  */
-class m180112_141250_create_motherboadrs_x_external_ports_table extends Migration
+class m180112_141250_create_motherboards_x_external_ports_table extends Migration
 {
     /**
      * @inheritdoc
@@ -15,22 +15,22 @@ class m180112_141250_create_motherboadrs_x_external_ports_table extends Migratio
         if (APP_MODE === 'legacy') {
 
             $this->execute(
-                'CREATE TABLE `motherboadrs_x_external_ports` (
+                'CREATE TABLE `motherboards_x_external_ports` (
                     `motherboard_id` int(11) NOT NULL,
                     `external_port_id` int(11) NOT NULL,
                     `quantity` int(11) NOT NULL
                 )'
             );
 
-            $this->execute('ALTER TABLE `motherboadrs_x_external_ports` ADD UNIQUE INDEX `idx-unique-mb_x_external_ports-mb_id-external_port_id` (`motherboard_id`, `external_port_id`)');
+            $this->execute('ALTER TABLE `motherboards_x_external_ports` ADD UNIQUE INDEX `idx-unique-mb_x_external_ports-mb_id-external_port_id` (`motherboard_id`, `external_port_id`)');
 
-            $this->execute('ALTER TABLE `motherboadrs_x_external_ports` ADD CONSTRAINT `fk-motherboadrs_x_external_ports-motherboard_id` FOREIGN KEY (`motherboard_id`) REFERENCES `motherboards` (`id`) ON DELETE CASCADE');
+            $this->execute('ALTER TABLE `motherboards_x_external_ports` ADD CONSTRAINT `fk-motherboards_x_external_ports-motherboard_id` FOREIGN KEY (`motherboard_id`) REFERENCES `motherboards` (`id`) ON DELETE CASCADE');
 
-            $this->execute('ALTER TABLE `motherboadrs_x_external_ports` ADD CONSTRAINT `fk-motherboadrs_x_external_ports-external_port_id` FOREIGN KEY (`external_port_id`) REFERENCES `external_ports` (`id`) ON DELETE CASCADE');
+            $this->execute('ALTER TABLE `motherboards_x_external_ports` ADD CONSTRAINT `fk-motherboards_x_external_ports-external_port_id` FOREIGN KEY (`external_port_id`) REFERENCES `external_ports` (`id`) ON DELETE CASCADE');
 
         } else {
 
-            $this->createTable('motherboadrs_x_external_ports', [
+            $this->createTable('motherboards_x_external_ports', [
                 'motherboard_id'   => $this->integer()->notNull(),
                 'external_port_id' => $this->integer()->notNull(),
                 'quantity'         => $this->integer()->notNull(),
@@ -38,14 +38,14 @@ class m180112_141250_create_motherboadrs_x_external_ports_table extends Migratio
 
             $this->createIndex(
                 'idx-unique-mb_x_external_ports-mb_id-external_port_id',
-                'motherboadrs_x_external_ports',
+                'motherboards_x_external_ports',
                 ['motherboard_id', 'external_port_id'],
                 true
             );
 
             $this->addForeignKey(
-                'fk-motherboadrs_x_external_ports-motherboard_id',
-                'motherboadrs_x_external_ports',
+                'fk-motherboards_x_external_ports-motherboard_id',
+                'motherboards_x_external_ports',
                 'motherboard_id',
                 'motherboards',
                 'id',
@@ -53,8 +53,8 @@ class m180112_141250_create_motherboadrs_x_external_ports_table extends Migratio
             );
 
             $this->addForeignKey(
-                'fk-motherboadrs_x_external_ports-external_port_id',
-                'motherboadrs_x_external_ports',
+                'fk-motherboards_x_external_ports-external_port_id',
+                'motherboards_x_external_ports',
                 'external_port_id',
                 'external_ports',
                 'id',
@@ -72,12 +72,12 @@ class m180112_141250_create_motherboadrs_x_external_ports_table extends Migratio
         if (APP_MODE === 'legacy') {
 
             $this->execute(
-                'DROP TABLE `motherboadrs_x_external_ports`'
+                'DROP TABLE `motherboards_x_external_ports`'
             );
 
         } else {
 
-            $this->dropTable('motherboadrs_x_external_ports');
+            $this->dropTable('motherboards_x_external_ports');
 
         }
     }
